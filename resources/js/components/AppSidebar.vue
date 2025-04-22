b1beb78bb38b9ee43fd6af0d28e581d227603166
Govinda Roy <govindaroy.ofc94@gmail.com>
2025-04-22 18:36:30 +0600

    Update

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -5,7 +5,7 @@ import NavUser from '@/components/NavUser.vue';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/vue3';
-import { BookOpen, Folder, LayoutGrid, Paperclip, Clapperboard, MonitorCog, MonitorStop, ReceiptText, Users, Image } from 'lucide-vue-next';
+import { BookOpen, Folder, LayoutGrid, Paperclip, Clapperboard, MonitorCog, MonitorStop, ReceiptText, Users, Image, CircleDollarSign } from 'lucide-vue-next';
 import AppLogo from './AppLogo.vue';
 
 // const { url } = usePage();
@@ -59,6 +59,11 @@ const footerNavItems: NavItem[] = [
     //     href: 'https://laravel.com/docs/starter-kits',
     //     icon: BookOpen,
     // },
+    {
+        title: 'Clients',
+        href: '/clients',
+        icon: CircleDollarSign,
+    },
     {
         title: 'Users',
         href: '/users',
