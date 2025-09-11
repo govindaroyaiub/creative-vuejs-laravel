9fcbbd5e021a6d082b7a023c369fb5a22a932dbe
Govinda Roy <govindaroy.ofc94@gmail.com>
2025-09-11 18:26:55 +0600

    Update

--- app/Models/Bill.php
+++ app/Models/Bill.php
@@ -3,13 +3,27 @@
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions;
 
 class Bill extends Model
 {
+    use LogsActivity;
+
+    protected static $logAttributes = ['*']; // logs all attributes
+    protected static $logName = 'Bill'; // name for this log
+
     protected $fillable = ['name', 'client', 'total_amount'];
 
     public function subBills(): HasMany
     {
         return $this->hasMany(SubBill::class);
+    }
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logAll()
+            ->useLogName('Bill');
     }
 }
