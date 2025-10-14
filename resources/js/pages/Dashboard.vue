5c7b4feb8a570a5a6e602e2cf612dc259d40d9c8
Govinda Roy <govindaroy.ofc94@gmail.com>
2025-10-14 23:54:17 +0600

    Update

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -415,7 +415,7 @@ const formatNumber = (num: number) => {
 
                         <!-- World Clocks - Responsive Layout -->
                         <div
-                            class="bg-gradient-to-r from-indigo-300 to-blue-100 dark:from-neutral-800 dark:to-neutral-800 rounded-xl p-3 md:p-4">
+                            class="bg-gradient-to-r from-indigo-300 to-blue-300 dark:from-neutral-800 dark:to-neutral-800 rounded-xl p-3 md:p-4">
                             <!-- Mobile: Stacked Layout -->
                             <div class="flex flex-col sm:flex-row gap-3 sm:gap-4">
                                 <!-- Bangladesh -->
