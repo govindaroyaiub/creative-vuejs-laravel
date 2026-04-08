77146634cde0845dd10dc70cabfa11d1792dc164
Govinda Roy <govindaroy.ofc94@gmail.com>
2026-04-08 14:31:55 +0600

    Removed the bg from dark mode for the avatar

--- resources/js/components/AppSidebarHeader.vue
+++ resources/js/components/AppSidebarHeader.vue
@@ -46,7 +46,7 @@ const firstName = computed(() => user.name.split(' ')[0]);
                         <Avatar class="h-8 w-8 rounded-full">
                             <AvatarImage v-if="showAvatar" :src="user.avatar" :alt="user.name" />
                             <AvatarFallback
-                                class="text-xs font-mono font-semibold text-black dark:text-white dark:bg-black">
+                                class="text-xs font-mono font-semibold text-black dark:text-white">
                                 {{ getInitials(user.name) }}
                             </AvatarFallback>
                         </Avatar>
